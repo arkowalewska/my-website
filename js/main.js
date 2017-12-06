09c6383e6927034def8964d7f0c994f6c98d127a
Alexandra Kowalewska <kowalewska.alexandra@gmail.com>
2017-12-06 02:37:31 +0100

    anchor changes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
-$(function(){
-	
-	/* Efekt Smooth Scroll */
-	
-	$('a[href*="#"]:not([href="#"])').click(function(){
-		console.log('test');
-		$('html, body').animate( { scrollTop: $(this.hash).offset().top - 74 }, 500 );
-	});
+$(function () {
 
-	/* Scroll menu color change */
-	
-	if($(window).scrollTop() >= 50){
-		$('#main-nav').addClass('scroll');
-	}
-	
-	$(window).scroll(function(){
-		if($(window).scrollTop() >= 50 ) {
-			$('#main-nav').addClass('scroll');
-			$('#main-nav').css({'transition':'all 0.3s'})
-		} else {
-			$('#main-nav').removeClass('scroll');
-		}
-	});
-	
+    /* Efekt Smooth Scroll */
+
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        console.log('test');
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top - 74
+        }, 500);
+    });
+
+    /* Scroll menu color change */
+
+    if ($(window).scrollTop() >= 50) {
+        $('#main-nav').addClass('scroll');
+    }
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >= 50) {
+            $('#main-nav').addClass('scroll');
+            $('#main-nav').css({
+                'transition': 'all 0.3s'
+            })
+        } else {
+            $('#main-nav').removeClass('scroll');
+        }
+    });
+
+    /*$('.nav a').on('click', function () {
+        $('.navbar-toggler').click();
+    });*/
+
 });
